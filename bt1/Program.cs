cb8343f0fed7c458014c92736dee11a3aadcd3e1
hv <hv@PM309-15>
2023-03-26 09:49:30 +0700

    Add project files.

---
+++ bt1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so nguyen N = ");
+            int N = int.Parse(Console.ReadLine());
+            int giai_thua=1;
+            int tong = 0;
+            for (int i=0; i<N; i++)
+            {
+                for (int j=N-i;j>=1;j--)
+                {
+                    //Console.WriteLine("J={0}",N);
+                    giai_thua=giai_thua*j;
+                    //Console.WriteLine("Giai thua = {0}",giai_thua);   
+                }
+                tong+=giai_thua;
+                //Console.WriteLine("Tong={0}",tong);
+                giai_thua = 1;
+            }
+            Console.WriteLine("Tong cac giai thua tu 1 den N la {0}", tong);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ bt2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //int N = int.Parse(Console.ReadLine());
+                //bool laSNT=true;
+                //string result = "";
+                //int tong = 0;
+                Console.WriteLine("Nhap phim Esc de thoat, nhap phim bat ky de tiep tuc");
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+                else
+                {
+                    bool laSNT = true;
+                    string result = "";
+                    int tong = 0;
+                    Console.WriteLine("<------------------------------------->");
+                    Console.Write("Nhap so nguyen N = ");
+                    int N = int.Parse(Console.ReadLine());
+                    for (int j = 2; j <= N; j++)
+                    {
+                        for (int i = 2; i < j; i++)
+                        {
+                            if (j % i == 0)
+                            {
+                                laSNT = false;
+                            }
+
+                        }
+                        if (laSNT)
+                        {
+                            result = (j == 2) ? $"{j}" : $"{result}, {j}";
+                            tong += j;
+                        }
+                        laSNT = true;
+                    }
+                    Console.WriteLine("Cac songuyen to den N la {0}", result);
+                    Console.WriteLine("Tong cac so nguyen to la {0}", tong);
+                    Console.WriteLine("<------------------------------------->");
+                }
+            }
+        }
+    }
+}
